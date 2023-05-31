b9a39d5e065b1c27df828da2e7ba776afab63886
Yandall <jhonstevenkill22@gmail.com>
2023-05-31 18:19:27 -0500

    Next config change

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:slug*",
-        destination: `${process.env.API_HOST}:slug*`,
+        source: "/api/:path*",
+        destination: `${process.env.API_HOST}:path*`,
       },
     ];
   },
