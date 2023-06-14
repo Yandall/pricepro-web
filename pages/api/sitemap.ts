999e2b11ed0f1be9496d03cec486a27b39ab8ac4
Yandall <jhonstevenkill22@gmail.com>
2023-06-13 22:24:15 -0500

    Typo in sitemap

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -37,7 +37,7 @@ export default async function handler(
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
     <url>
-      <loc>https://${host}/</loc>a
+      <loc>https://${host}/</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
     </url>
     <url>
