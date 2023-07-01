d9e24f4f753e49f7546654e470cb20a8373027be
Jhon Steven Agudelo Rodríguez <49245907+Yandall@users.noreply.github.com>
2023-06-30 20:45:28 -0500

    Added Makro icon

--- components/StoreIcon.tsx
+++ components/StoreIcon.tsx
@@ -2,6 +2,7 @@ import exitoIcon from "@/public/store-exito-icon.webp";
 import jumboIcon from "@/public/store-jumbo-icon.webp";
 import carullaIcon from "@/public/store-carulla-icon.webp";
 import d1Icon from "@/public/store-d1-icon.webp"
+import makroIcon from "@/public/store-makro-logo.webp"
 
 import { StaticImageData } from "next/image";
 import { Image, Tooltip } from "@mantine/core";
@@ -26,6 +27,8 @@ export default function StoreIcon({ store, size }: Props) {
     case "D1":
       icon = d1Icon
       break;
+    case "Makro":
+      icon = makroIcon
   }
   return (
     <Tooltip
@@ -37,8 +40,8 @@ export default function StoreIcon({ store, size }: Props) {
         <Image
           src={icon.src}
           alt={store}
+          height="auto"
           width={size ?? 20}
-          height={size ?? 20}
         />
       )}
     </Tooltip>
